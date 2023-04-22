385aef3703b081fa7cc94288749b2dcc562165ae
cgache2 <mystfication@yahoo.com>
2023-04-22 00:47:35 -0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig(({mode}) => {
 
   return {
     plugins: [svelte(), htmlPlugin()],
+    base: '/sexy-pokemon-trainer-cards/'
     server: {
       watch: {
         usePolling: false
